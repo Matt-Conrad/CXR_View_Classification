155ceb1eca2aa8f7bf2bcaba6a8d2b7e474970a6
Matt-Conrad <mattgrayconrad@gmail.com>
2020-01-27 12:45:50 -0500

    Added functionality that will check for the TGZ was downloaded properly. Also, changed the download_dataset.py to a class structure instead of a library of functions.

--- download_dataset.py
+++ download_dataset.py
@@ -4,30 +4,64 @@ import tarfile
 import os
 import logging
 
-def download_dataset(url):
-    # Start download
-    logging.info('Downloading dataset from %s', url)
+EXPECTED_SIZES = {
+        'NLMCXR_subset_dataset.tgz': 88320855,
+        'NLMCXR_dcm.tgz': 80694582486
+    }
 
-    filename = url.split("/")[-1]
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
-                if chunk: # filter out keep-alive new chunks
-                    f.write(chunk)
+class DatasetController:
+    """Controls logic of getting the dataset from online sources."""
+    def __init__(self, url):
+        self.url = url
+        self.filename = url.split("/")[-1]
+        self.expected_size = EXPECTED_SIZES[self.filename]
 
-    # if os.path.getsize('NLMCXR_dcm.tgz') != 80694582486:
-    #     raise IOError('NLMCXR_dcm.tgz did not download properly!')
+    def get_dataset(self):
+        """Attempt to get the dataset TGZ as many times as it takes."""
+        logging.info('Checking if %s already exists', self.filename)
+        if os.path.isfile(self.filename):
+            logging.info('%s already exists', self.filename)
+            logging.info('Checking if %s was downloaded properly', self.filename)
+            if os.path.getsize(self.filename) == self.expected_size:
+                logging.info('%s was downloaded properly', self.filename)
+            else:
+                logging.warning('%s was NOT downloaded properly', self.filename)
+                logging.info('Removing %s', self.filename)
+                os.remove(self.filename)
+                logging.info('Successfully removed %s', self.filename)
+                self.download_dataset()
+        else:
+            logging.info('%s does not exist', self.filename)
+            self.download_dataset()
 
-    logging.info('Download successful')
+    def download_dataset(self):
+        """Download the dataset, invoke the checks in get_dataset after."""
+        # Start download
+        logging.info('Downloading dataset from %s', self.url)
+        
+        with requests.get(self.url, stream=True) as r:
+            r.raise_for_status()
+            with open(self.filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192): 
+                    if chunk: # filter out keep-alive new chunks
+                        f.write(chunk)
 
-    return filename
+        self.get_dataset()
+        return self.filename
 
-def unpack(filename):
-    logging.info('Unpacking dataset from %s', filename)
-    tf = tarfile.open(filename)
-    folder_name = filename.split('.')[0]
-    tf.extractall(path='./' + folder_name)
-    logging.info('Done unpacking')
+    def unpack(self):
+        logging.info('Unpacking dataset from %s', self.filename)
+        tf = tarfile.open(self.filename)
+        folder_name = self.filename.split('.')[0]
+        tf.extractall(path='./' + folder_name)
+        logging.info('Done unpacking')
 
-    return os.path.dirname(os.path.abspath(__file__)) + '/' + folder_name
+        return os.path.dirname(os.path.abspath(__file__)) + '/' + folder_name
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='download_dataset.log', level=logging.INFO)
+    url = 'https://github.com/Matt-Conrad/CXR_View_Classification/raw/master/NLMCXR_subset_dataset.tgz'
+    controller = DatasetController(url)
+    controller.get_dataset()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """Contains the software coordinating the logic of the application."""
 import logging
-from download_dataset import download_dataset, unpack
+from download_dataset import DatasetController
 from DicomToDatabase.dicom_to_db import dicom_to_db
 from calculate_features import calculate_features
 from LabelImages import run_app
@@ -18,16 +18,16 @@ class Controller:
         self.columns_info = 'columns_info.json'
         self.classifier = None
         self.url = 'https://github.com/Matt-Conrad/CXR_View_Classification/raw/master/NLMCXR_subset_dataset.tgz'
-        self.tgz_filename = None
+        self.dataset_controller = DatasetController(self.url)
         logging.info('Controller initialized')
 
     def download_dataset(self):
         """Download the dataset (tgz format) from the public repository."""
-        self.tgz_filename = download_dataset(self.url)
+        self.dataset_controller.get_dataset()
 
     def unpack_dataset(self):
         """Unpack the dataset from the tgz file."""
-        folder = unpack(self.tgz_filename)
+        folder = self.dataset_controller.unpack()
         config.update_config_file(filename=self.config_file_name, section='dicom_folder', key='folder_path', value=folder)
 
     def store_metadata(self):
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     controller = Controller()
     controller.download_dataset()
     controller.unpack_dataset()
-    controller.store_metadata()
-    controller.calculate_features()
-    controller.label_images()
-    controller.classification()
+    # controller.store_metadata()
+    # controller.calculate_features()
+    # controller.label_images()
+    # controller.classification()
