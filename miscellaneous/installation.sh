2d5aa017eaf1866eddb9e5c3b5a175e827dee928
Matt-Conrad <mattgrayconrad@gmail.com>
2020-12-10 11:49:50 -0500

    Made pipeline improvements

--- miscellaneous/installation.sh
+++ miscellaneous/installation.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 # Move this one to run after the VM set up no matter what
-endpoint1=matt@192.168.61.131
-sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S /usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other'
+username=$(<./miscellaneous/JenkinsUsername)
 
-sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/postgresSetup.sh'
-sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/postgresSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/pythonSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/pythonSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/cppSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/cppSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/combinedSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint1 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/combinedSetup.sh'
+echo "Username"
+echo $username
+echo "Authorization: Basic ${username}"
+echo curl 'http://127.0.0.1:8697/api/vms/0G9LFS4SVBVOVO33L6NBO3K4N9GV3F0U/ip' -X GET --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' --header "Authorization: Basic ${username}"
 
+curl 'http://127.0.0.1:8697/api/vms/0G9LFS4SVBVOVO33L6NBO3K4N9GV3F0U/power' -X PUT --header 'Content-Type: application/vnd.vmware.vmw.rest-v1+json' --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' --header "Authorization: Basic ${username}" -d 'on'
 
+IP=$(curl 'http://127.0.0.1:8697/api/vms/0G9LFS4SVBVOVO33L6NBO3K4N9GV3F0U/ip' -X GET --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' --header "Authorization: Basic ${username}"| jq '.ip' | tr -d '"')
 
-endpoint2=matt@192.168.61.132
-sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S /usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other'
+echo "IP Variable"
+echo $IP
 
-sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/postgresSetup.sh'
-sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/postgresSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/pythonSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/pythonSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/cppSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/cppSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/combinedSetup.sh'
-# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint2 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/combinedSetup.sh'
+sleep 15
+echo "Keygen"
+ssh-keygen -R $IP
+
+sleep 15
+
+# endpoint=matt@$IP
+echo "SSH Calls"
+sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S /usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other'
+
+sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/postgresSetup.sh'
+sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/postgresSetup.sh'
+# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/pythonSetup.sh'
+# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/pythonSetup.sh'
+# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/cppSetup.sh'
+# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/cppSetup.sh'
+# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S chmod u+x /mnt/hgfs/SharedFolder_Guest/miscellaneous/combinedSetup.sh'
+# sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $IP -l matt 'echo password | sudo -S /mnt/hgfs/SharedFolder_Guest/miscellaneous/combinedSetup.sh'
+
+curl 'http://127.0.0.1:8697/api/vms/0G9LFS4SVBVOVO33L6NBO3K4N9GV3F0U/power' -X PUT --header 'Content-Type: application/vnd.vmware.vmw.rest-v1+json' --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' --header "Authorization: Basic ${username}" -d 'off'
