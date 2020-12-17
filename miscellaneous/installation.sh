87c1beb9fd8356ac4e2dfe97bb5b0341d03b2462
Matt-Conrad <mattgrayconrad@gmail.com>
2020-12-17 12:11:43 -0500

    Moved all steps for Set Up VM into installation.sh and added code to wait for vmrest to start up before turning off VM if it's on

--- miscellaneous/installation.sh
+++ miscellaneous/installation.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# Move this one to run after the VM set up no matter what
+
+# Start up vmrest API
+nohup vmrest &
+
+# Wait for vmrest to start up
+while ! echo exit | nc 127.0.0.1 8697; do sleep 1; done
 
 # Must set up credentials in Jenkins
 auth=$(echo -n ${VMREST_CREDS} | base64)
 
+# Get VM ID
 id=null
 while [ $id == null ]
 do 
@@ -11,20 +17,35 @@ do
  
     id=$(echo $vmList | jq --arg VM_DESTINATION "$VM_DESTINATION" '.[] | select(.path | contains($VM_DESTINATION)) | .id')
 done
-
 id=$(echo $id | tr -d '"')
+
+# Turn off VM
+curl "http://127.0.0.1:8697/api/vms/${id}/power" -X PUT --header 'Content-Type: application/vnd.vmware.vmw.rest-v1+json' --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' --header "Authorization: Basic ${auth}" -d 'off'
+
+# Copy snapshot VM
+rm -rf ${VM_DESTINATION}
+cp -r ${VM_SOURCE} ${VM_DESTINATION}
+
+# Replace shared folder contents with most recent build
+rm -rf ${SHARED_FOLDER_HOST_PATH}/*
+cp -r `ls --ignore=.*` ${SHARED_FOLDER_HOST_PATH}
+
+# Turn on VM
 curl "http://127.0.0.1:8697/api/vms/${id}/power" -X PUT --header 'Content-Type: application/vnd.vmware.vmw.rest-v1+json' --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' --header "Authorization: Basic ${auth}" -d 'on'
 
-online=null
-while [ $online == null ]
+# Get IP of VM
+IP=null
+while [ $IP == null ]
 do 
-    online=$(curl "http://127.0.0.1:8697/api/vms/${id}/ip" -X GET --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' --header "Authorization: Basic ${auth}" | jq '.ip' )
+    IP=$(curl "http://127.0.0.1:8697/api/vms/${id}/ip" -X GET --header 'Accept: application/vnd.vmware.vmw.rest-v1+json' --header "Authorization: Basic ${auth}" | jq '.ip' )
     sleep 1
 done
+IP=$(echo ${IP} | tr -d '"')
 
-IP=$(echo ${online} | tr -d '"')
+# Update ssh key
 ssh-keygen -R $IP
 
+# Run scripts on VM using SSH
 endpoint=matt@$IP
 sshpass -p 'password' ssh -o 'StrictHostKeyChecking no' $endpoint 'echo password | sudo -S /usr/bin/vmhgfs-fuse .host:/ /mnt/hgfs -o subtype=vmhgfs-fuse,allow_other'
 
