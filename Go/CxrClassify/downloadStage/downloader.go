683deb8d6dbbf0a9975cd806cbc3a04b15a9d52b
Matt-Conrad <mattgrayconrad@gmail.com>
2022-05-13 18:20:50 -0400

    - Add goroutines for updating the download progress bar

--- Go/CxrClassify/downloadStage/downloader.go
+++ Go/CxrClassify/downloadStage/downloader.go
@@ -5,8 +5,9 @@ import (
 	"CxrClassify/databaseHandler"
 	"CxrClassify/runnable"
 	"log"
+	"sync"
+	"time"
 
-	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -32,6 +33,13 @@ func (d *Downloader) Setup(configHandler *configHandler.ConfigHandler, databaseH
 }
 
 func (d Downloader) Run() {
+	k, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer k.Close()
+	log.SetOutput(k)
+
 	log.Println("Checking if {} already exists")
 	info, err := os.Stat(d.FilenameAbsPath)
 	if err == nil && !info.IsDir() {
@@ -47,54 +55,112 @@ func (d Downloader) Run() {
 				log.Println("FAILED")
 			}
 			log.Println("Successfully removed {}")
-			d.downloadDataset()
 		}
 	} else {
 		log.Println("{} does not exist")
-		d.downloadDataset()
+		messages := make(chan string)
+
+		msg := ""
+
+		go d.downloadDataset(messages)
+
+		log.Println("asdf")
+
+		for msg == "" {
+			log.Println("dfsdf")
+			select {
+			case msg = <-messages:
+				log.Println(msg)
+				d.AttemptUpdateText(msg)
+			default:
+				log.Println("f")
+			}
+		}
+
+		// d.AttemptUpdateText(msg)
 	}
-	d.AttemptUpdateProBarValue(1)
 	// d.AttemptUpdateProBarValue(int(d.getTgzSize()))
-	d.AttemptUpdateText("Image download complete")
+	// d.AttemptUpdateText("Image yo complete")
 	d.Finished()
 }
 
-func (d Downloader) downloadDataset() int {
+func (d Downloader) downloadDataset(msg chan string) int {
 	log.Printf("Downloading dataset from: %s", d.ConfigHandler.GetUrl())
 
+	msg <- "Downloading images"
 	d.AttemptUpdateText("Downloading images")
-	// d.AttemptUpdateProBarBounds(0, int(d.getTgzMax()))
+	d.AttemptUpdateProBarBounds(0, int(d.getTgzMax()))
 
-	d.AttemptUpdateProBarBounds(0, 1)
+	// Writer the body to file
+	var wg sync.WaitGroup
 
+	wg.Add(1)
+	go d.download(&wg)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		exists := false
+		for !exists {
+			_, error := os.Stat("NLMCXR_subset_dataset.tgz")
+
+			// check if error is "file not exists"
+			if os.IsNotExist(error) {
+				log.Printf("file does not exist\n")
+			} else {
+				log.Printf("file exist\n")
+				exists = true
+			}
+		}
+
+		var downloaded int64 = -1
+		for downloaded < d.getTgzMax() {
+			downloaded = d.getTgzSize()
+			d.update(downloaded)
+			time.Sleep(time.Second)
+		}
+	}()
+
+	wg.Wait()
+
+	return 0
+}
+
+func (d Downloader) download(wg *sync.WaitGroup) int {
 	// Create the file
 	out, err := os.Create(d.FilenameAbsPath)
 	if err != nil {
-		fmt.Println("1")
+		log.Println("1")
 		return 1
 	}
 	defer out.Close()
+	log.Println("1")
 
 	// Get the data
 	resp, err := http.Get(d.ConfigHandler.GetUrl())
 	if err != nil {
-		fmt.Println("2")
+		log.Println("2")
 		return 1
 	}
 	defer resp.Body.Close()
+	log.Println("2")
 
 	// Check server response
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("3")
+		log.Println("3")
 		return 1
 	}
+	log.Println("3")
 
-	// Writer the body to file
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		fmt.Println("4")
+		log.Println("4")
 		return 1
 	}
+	log.Println("4")
+
+	wg.Done()
 
 	return 0
 }
@@ -102,7 +168,7 @@ func (d Downloader) downloadDataset() int {
 func (d Downloader) getTgzSize() int64 {
 	fi, err := os.Stat(d.FilenameAbsPath)
 	if err != nil {
-		fmt.Println("5")
+		log.Println("5")
 	}
 
 	if d.DatasetType == "full_set" {
@@ -112,6 +178,12 @@ func (d Downloader) getTgzSize() int64 {
 	} else {
 		return 0
 	}
+}
+
+func (d *Downloader) update(value int64) {
+	log.Println(value)
+	// d.AttemptUpdateProBarValue(int(value))
+	d.AttemptUpdateProBarValue(int(d.getTgzSize()))
 }
 
 func (d Downloader) getTgzMax() int64 {
